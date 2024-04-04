fac859099698e31073bc18a0d18acebcaa80b096
Heorhi Davydau <heorhid@gmail.com>
2024-04-04 11:08:56 +0200

    global refactor

--- js/loader.js
+++ js/loader.js
@@ -1,19 +1,14 @@
-chrome.storage.local.get(null, function(storage) {
-    var styleNode = document.createElement("style");
+chrome.storage.local.get(null, (storage) => {
+    const styleNode = document.createElement("style");
     styleNode.id = "BetterAIS";
     styleNode.innerHTML = storage.themes[storage.selected];
     document.head.appendChild(styleNode);
-
-
-    // var scriptNode = document.createElement("script");
-    // scriptNode.setAttribute("src", `chrome-extension://${chrome.runtime.id}/js/script.js`);
-    // document.body.appendChild(scriptNode);
 });
 
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    chrome.storage.local.get(null, function(storage) {
-        var node = document.getElementById("BetterAIS");
+chrome.storage.onChanged.addListener(() => {
+    chrome.storage.local.get(null, (storage) => {
+        const node = document.getElementById("BetterAIS");
         node.innerHTML = storage.themes[storage.selected];
     });
 });
--- js/popup.js
+++ js/popup.js
@@ -1,70 +1,35 @@
+/**
+ * Loads all available themes from storage and populates the select dropdown.
+ */
+function loadThemes() {
+  chrome.storage.local.get(null, (storage) => {
+    const themeSelect = document.getElementById("select");
+    Object.keys(storage.themes).forEach((theme) => {
+      const option = document.createElement("option");
+      option.value = theme;
+      option.innerText = theme;
+      themeSelect.appendChild(option);
+    });
+  });
+}
 
-// Load all themes to select
-chrome.storage.local.get(null, function(storage) {
-    // Selected first
-    var node = document.createElement("option");
-    node.value = storage.selected;
-    node.innerHTML = storage.selected;
-    document.getElementById("select").appendChild(node);
-    delete storage.themes[storage.selected];
-    // 
-    for (let theme of Object.keys(storage.themes))
-    {
-        var node = document.createElement("option");
-        node.value = theme;
-        node.innerHTML = theme;
-        document.getElementById("select").appendChild(node);
-    }
-});
+/**
+ * Sets the user-selected theme in storage.
+ */
+function setSelectedTheme() {
+  const selectedTheme = document.getElementById("select").value;
+  chrome.storage.local.set({ "selected": selectedTheme });
+}
 
+/**
+ * Resets the theme to a default value.
+ * @param {string} defaultTheme - The default theme to reset to.
+ */
+function resetToDefaultTheme(defaultTheme = "Dark") {
+  chrome.storage.local.set({ "selected": defaultTheme });
+  document.getElementById("select").value = defaultTheme;
+}
 
-
-// Set selected data
-document.getElementById("apply").onclick = function(){ chrome.storage.local.set({"selected": document.getElementById("select").value}) };
-
-
-document.getElementById("defaultStyles").onclick = function(){ 
-
-    var defaultThemes = {
-        "Nord": ["url", `chrome-extension://${chrome.runtime.id}/css/nord.css`],
-        "Neon": ["url", `chrome-extension://${chrome.runtime.id}/css/neon.css`],
-        "Dark": ["url", `chrome-extension://${chrome.runtime.id}/css/dark.css`],
-        "Incognito": ["url", `chrome-extension://${chrome.runtime.id}/css/incognito.css`],
-        "AIS Default": ["text", ""]
-    }
-
-    // console.log(chrome.storage.local);
-    
-
-    var themes = {};
-    for (let themeName of Object.keys(defaultThemes))
-    {
-        if (defaultThemes[themeName][0] == "url")
-        {
-            fetch(defaultThemes[themeName][1])
-            .then((resp) => {
-                    return resp.text()
-                })
-            .then((data) => {
-                themes[themeName] = data;
-            })
-        }
-        if (defaultThemes[themeName][0] == "text")
-        {
-            themes[themeName] = defaultThemes[themeName][1];
-        }
-    }
-
-    var checkerInterval = setInterval(() => {
-        if (Object.keys(themes).length == Object.keys(defaultThemes).length)
-        {
-            
-            chrome.storage.local.set({themes: themes, selected: "Neon"}, function() {
-                chrome.storage.local.get(null, function(storage) {
-                    console.log(storage);
-                });
-            });
-            clearInterval(checkerInterval);
-        }
-    }, 1);
-};
+document.addEventListener("DOMContentLoaded", loadThemes);
+document.getElementById("apply").addEventListener("click", setSelectedTheme);
+document.getElementById("defaultStyles").addEventListener("click", () => resetToDefaultTheme());
